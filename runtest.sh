6dd68c72f9e6900ea6bf9bf3aba927e9e9fd85a5
Casey Jordan <casey.jordan@jorsek.com>
2012-06-07 10:55:26 -0400

    Moved runtest utiltiy over to using XML configurations instead of command line args.

--- runtest.sh
+++ runtest.sh
@@ -1,6 +1,54 @@
-#! /bin/sh
+#!/bin/bash
 
-USAGE="Usage: `basename $0` [-hutidD] test-plan"
+print_usage(){
+	USAGE="Usage: `basename $0` [options] <test-config>.xml"
+
+	echo -e "$USAGE\n"
+            
+             	echo -e " -----[Options]-----";
+           	echo -e "";
+           	echo -e "	-o	<file>             Write generated test-plan to file and don't start tests.";
+          	echo -e "	-j	<jmx-url>          The url to monitor for eXists jmx status. Default is $JMXURL";
+          	echo -e ""
+}
+
+get_real_script_path()
+{
+	case $0 in
+    /*)
+        SCRIPT="$0"
+        ;;
+    *)
+        PWD=`pwd`
+        SCRIPT="$PWD/$0"
+        ;;
+	esac
+
+	# Change spaces to ":" so the tokens can be parsed.
+	SCRIPT=`echo $SCRIPT | sed -e 's; ;:;g'`
+	# Get the real path to this script, resolving any symbolic links
+	TOKENS=`echo $SCRIPT | sed -e 's;/; ;g'`
+	REALPATH=
+	for C in $TOKENS; do
+	    REALPATH="$REALPATH/$C"
+	    while [ -h "$REALPATH" ] ; do
+	        LS="`ls -ld "$REALPATH"`"
+	        LINK="`expr "$LS" : '.*-> \(.*\)$'`"
+	        if expr "$LINK" : '/.*' > /dev/null; then
+	            REALPATH="$LINK"
+	        else
+	            REALPATH="`dirname "$REALPATH"`""/$LINK"
+	        fi
+	    done
+	done
+	# Change ":" chars back to spaces.
+	REALPATH=`echo $REALPATH | sed -e 's;:; ;g'`
+	
+	echo $REALPATH
+}
+
+SCRIPT_PATH=$(get_real_script_path $0)
+SCRIPT_DIR=$(dirname $SCRIPT_PATH)
 
 on_die()
 {
@@ -15,98 +63,78 @@ on_die()
 	exit 0
 }
 
+DEBUG="true";
 
-
-NUM_USERS=""; 		# Total number of users to simulate
-TEST_TIME=""; 		# Time in seconds
-DELAY="";		   		# Number of seconds between users ariving
-INTERVAL="";		    # Interval in between each request in seconds
-DUMP_TRAFFIC="false"
-EXIST_SERVER="localhost";
-EXIST_PORT="8080";
-
+JMXURL="localhost:8080/exist/status?c=locking";
 # Parse command line options.
-while getopts hu:t:i:d:D OPT; do
+while getopts ho:m: OPT; do
     case "$OPT" in
         h)
-            echo -n "$USAGE\n"
-           	echo -n "	[Options] - Options modify the default test plan behavior\n";
-           	echo -n "\n\n";
-           	echo -n "	-n	max_users (Force max number of simulated users)\n";
-           	echo -n "	-t	test_time (Force max test run time)\n";
-           	echo -n "	-d	request_delay (Force delay between requests)\n";
-           	echo -n "	-i	users_interval (Force interval between user arrival in seconds)\n";
-           	echo -n "	-D (Dump all traffic to tsung.dump)\n";
-           	#echo -n "	-G (Automatically generate report after completion and launch in firefox)\n";
+            print_usage
             exit 0
+           ;;
+        m)
+            TEST_MODIFIER=$OPTARG
             ;;
-        u)
-            NUM_USERS=$OPTARG
-            ;;
-          
-        t)
-            TEST_TIME=$OPTARG
-            ;;
-        i)
-            INTERVAL=$OPTARG
-            ;;
-        d)
-           
-            	DELAY=$OPTARG
-            ;;
-          D)
-           
-            	DUMP_TRAFFIC="true"
-            ;;
+            
+	     j)
+	        JMXURL=$OPTARG
+	        ;;
+        o)
+        	WRITE_TEST_PLAN=$OPTARG;
+        ;;
+       
         \?)
             # getopts issues an error message
-            echo $USAGE >&2
+            
+            echo "" >&2
+            
+            print_usage
             exit 1
             ;;
     esac
 done
 
+
+
 # Remove the switches we parsed above.
 shift `expr $OPTIND - 1`
 
-#echo -n "\n";
-#echo -n "NUM_USERS:$NUM_USERS\n";
-#echo -n "TEST_TIME:$TEST_TIME\n";
-#echo -n "DELAY:$DELAY\n";
-#echo -n "INTERVAL:$INTERVAL\n";
-#echo -n "TEST-PLAN:$1\n\n";
 
 TEST_PLAN=$1;
-TEST_PLAN_FILENAME=$(basename $TEST_PLAN)
-TEST_DIR=$(dirname $TEST_PLAN)
-GEN_TEST_PLAN_NAME="generated-test-plan.xml"
-GEN_TEST_PLAN=$( readlink -f "$( dirname "$TEST_DIR/$GEN_TEST_PLAN_NAME" )" )/$( basename "$TEST_DIR/$GEN_TEST_PLAN_NAME" )
-echo $GEN_TEST_PLAN;
+
+if [ "$TEST_PLAN" = "" ] 
+then
+	echo -e "\nError: you must specify a test plan\n";
+	print_usage
+	exit 1;
+fi
+
+CUR_DATETIME=$(date '+%Y%m%d-%H%M')
 
 # Generate the test plan
-java  -jar ./lib/saxon/saxon9he.jar -o:"$GEN_TEST_PLAN" $1 lib/test-generator.xsl max_users="$NUM_USERS" test_time="$TEST_TIME" request_delay="$DELAY" interval="$INTERVAL"  dump_traffic="$DUMP_TRAFFIC"
+GEN_TEST_PLAN=$(java  -jar ./lib/saxon/saxon9he.jar $TEST_PLAN lib/test-modifier.xsl date-time="$CUR_DATETIME")
+
+
+TEST_DIR=$(dirname $GEN_TEST_PLAN)
+
 
 #Use datetime to find real log location for tsung, MUST BE IN FORMAT YYYYMMDD-HHMM
-CUR_DATETIME=$(date '+%Y%m%d-%H%M')
-LOG_DIR_REL="./logs/$TEST_PLAN_FILENAME";
-LOG_DIR=$( readlink -f $( dirname $LOG_DIR_REL ))
-REL_DTD_LOCATION="./lib/tsung-1.0.dtd";
 
-DTD_LOCATION=$( readlink -f "$( dirname "$REL_DTD_LOCATION" )" )/$( basename "$REL_DTD_LOCATION" );
+LOG_DIR="$SCRIPT_DIR/logs/";
+
+mkdir -p $LOG_DIR;
+
+DTD_LOCATION="$SCRIPT_DIR/lib/tsung-1.0.dtd";
 
 sed -i "1i<!DOCTYPE tsung SYSTEM \"$DTD_LOCATION\">" $GEN_TEST_PLAN;
 
-if [ ! -d $LOG_DIR ] 
-then 	
-	mkdir $LOG_DIR;	
-fi 
-
-TSUNG_LOG_ACTUALLY="$LOG_DIR/$CUR_DATETIME";
-
 log_exist_status()
 {
-	echo $(wget -T 100 -t 1 localhost:8080/exist/status?c=locking -O $STATUS_LOG -o /dev/stdout )
+	echo $(wget -T 100 -t 1 "$JMXURL" -O $STATUS_LOG -o /dev/stdout )
 }
+
+TSUNG_LOG_ACTUALLY="$LOG_DIR/$CUR_DATETIME";
 
 monitor_exist()
 {
@@ -123,12 +151,21 @@ monitor_exist()
 	if [ "$(echo $result | grep '200 OK' )" = "" ] 
 	then
 	
-		echo "WARNING: Could not retireve eXist JMX status. System may be unresponsive.";
+		echo -e "WARNING: Could not retireve eXist JMX status at: $JMXURL.\nSystem may be unresponsive.";
 	fi
 	
 	sleep 2
 	done
 }
+
+TEST_PLAN_DATA=$(cat $GEN_TEST_PLAN)
+
+if [ "$WRITE_TEST_PLAN" != "" ] 
+then
+	echo -e "\nWriting test plan to $WRITE_TEST_PLAN. Not running any tests\n\n";
+	echo $TEST_PLAN_DATA > $WRITE_TEST_PLAN;
+	exit
+fi
 
 trap 'on_die' TERM
 
@@ -136,9 +173,10 @@ monitor_exist &
 
 CUR_DIR=$(pwd);
 
+
 cd $TEST_DIR;
 
-tsung -f $GEN_TEST_PLAN -l $LOG_DIR -w 0 start &
+tsung -f "$GEN_TEST_PLAN" -l "$LOG_DIR" -w 0 start &
 
 wait
 
